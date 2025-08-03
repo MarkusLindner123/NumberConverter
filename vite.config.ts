5684aa86662d9dd226d1dd11400415f2bf32b16d
markuslindner123 <markuslindner1998@gmail.com>
2025-08-03 14:29:57 +0000

    shoudl work

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/NumberConverter/', // Dies ist der entscheidende Teil für GitHub Pages
-})
